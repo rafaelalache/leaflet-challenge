4cae456d3c45ca27d8fddf526ad8cb2033890043
Rafael Lewis <rafaelewis22@gmail.com>
2022-09-28 22:04:44 -0500

    initial commit

---
+++ static/js/logic.js
@@ -0,0 +1,38 @@
+//leaflett
+
+let myMap = L.map("map", {
+    center: [46.2276, 2.2137],
+    zoom: 15,
+  });
+  
+  let baseMaps = {
+    Satellite: street,
+    Grayscale: topo 
+
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data){
+console.log(data)
+//colors
+//circles
+
+})
